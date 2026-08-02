bc56684b679237d8252786e3d02967a3b8b47c8e
Menarawas <menaralintang.2019@student.uny.ac.id>
2026-08-02 14:25:58 +0700

    feat/user-service add module, service and controller employee and department

--- apps/user-service/src/app.module.ts
+++ apps/user-service/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DepartmentModule } from './modules/department/department.module';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { EmployeeService } from './controller/employee/employee.service';
 
 @Module({
-  imports: [],
+  imports: [DepartmentModule, EmployeeModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, EmployeeService],
 })
 export class AppModule {}
