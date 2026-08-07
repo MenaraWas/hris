054ddc55c5b502c790b7e64a64969a30c85e5739
Menarawas <menaralintang.2019@student.uny.ac.id>
2026-08-07 13:13:04 +0700

    add TCP, install and first config

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +15,19 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors();
 
+  //add tcp
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 4002,
+    }
+  })
+
+  await app.startAllMicroservices();
   await app.listen(process.env.PORT || 3002);
-  console.log('User service berjalan di port 3002');
+  console.log('User service HTTP berjalan di port 3002');
+  console.log('User service TCP berjalan di port 4002');
 }
 
 bootstrap();
